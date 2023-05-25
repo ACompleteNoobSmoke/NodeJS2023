ec0badc946abab3d795af3ef90e63d61822f173d
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-05-25 15:56:41 -0500

    Made custom package

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,5 +1,9 @@
 var _ = require('underscore');
+var lion = require('lion-noob007');
+
 
 var result = _.contains([1, 2, 3, 4, 5], 2);
 
 console.log(result);
+console.log(lion.addMethod(5, 15));
+console.log(lion.multiplyMethod(2, 20));
