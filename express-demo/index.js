9dcf5eb725ec3be4b88d47317246a34fd913d1dd
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-06-27 16:59:19 -0500

    Did some express practice

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,15 +1,34 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8008;
+
+const food = ['Spaghetti', 'Ramen Noodles', 'Jollof Rice', 'BBQ Hamburgers'];
 
 app.get('/', (req, res) => {
     res.send('Bruce Lee')
 })
 
 app.get('/api/food', (req, res) => {
-    const food = ['Spaghetti', 'Ramen Noodles', 'Jollof Rice']
     res.send(food)
+});
+
+app.post('/api/food', (req, res) => {
+    const foodItem = req.body.food;
+    var lowerCaseFood = food.map(f => f.toLowerCase());
+   if (lowerCaseFood.includes(foodItem.toLowerCase())) return res.status(400).send(`${foodItem} Already Exists`);
+    food.push(foodItem);
+    res.status(202).send(food);
 })
 
-app.listen(8008, () => {
-    console.log('Connected To Port')
+app.get('/api/food/:id', (req, res) => {
+    const foodID = req.params.id - 1;
+    var inRange = foodID in food;
+    if (!inRange) res.status(400).send('Incorrect Input');
+    res.json(food[foodID]);
 })
+
+app.listen(PORT, () => { console.log(`Connected To Port:${PORT}`)});
