7375168fb604c445fe4c3c55b582111fbdc44c9e
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-08-21 11:32:20 -0500

    Route updated

--- express-demo/routes/movie.js
+++ express-demo/routes/movie.js
@@ -35,6 +35,25 @@ app.post('/', async (req, res) => {
     }
 })
 
+app.delete('/:id', async (req, res) => {
+    try {
+        const {statusCode, message} = await movieService.deleteMovieByID(req);
+        res.status(statusCode).send(message);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+
+app.delete('/', async (req, res) => {
+    try {
+        const {error} = movieValidate(schema.titleSchema, req);
+        if (error) return res.status(404).send(error.details[0].message);
+        const {statusCode, message} = await movieService.deleteMovieByID(req);
+        res.status(statusCode).send(message);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
 
 
 module.exports = app;
